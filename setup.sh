5c144e060df44f2b7ec1303b5501d368669ada70
Cedric Grothues <cedricgrothues@gmail.com>
2021-02-10 21:48:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-for file in $(find . -maxdepth 1 -type f -name '.*' -not -name '.gitignore'); do
-  ln -sf "$(pwd)/$(basename $file)" "$HOME/$(basename $file)"
+for file in `find . -name ".*" -maxdepth 1 -type f -not -name '.gitignore'`; do
+  ln -sf `pwd`/`basename $file` $HOME/`basename $file`
 done
-
